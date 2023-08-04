0000b38865292538c4fc0297e0a7339f60282e8c
rhdedgar23 <96807850+rhdedgar23@users.noreply.github.com>
2023-08-03 18:11:43 -0600

    Update MergeSort.java

--- mergeSortAlgorithm/src/mergeSort/MergeSort.java
+++ mergeSortAlgorithm/src/mergeSort/MergeSort.java
@@ -26,6 +26,38 @@ public class MergeSort {
 		printArray(numbers);
 	}
 	
+	private static void printArray(int [] numbers) {
+		for(int i=0; i< numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+	}
+	
+	private static void mergeSort(int [] inputArray) {
+		
+		int inputLength= inputArray.length;
+		//First, we check the input array length.
+		//If the array is of length 0 or 1, we have come across an already sorted array. 
+		//Therefore we just return. 
+		if(inputLength< 2) {
+			return;
+		}
+		
+		//Else, just continue dividing the array. 
+		//Less memory and time used if we organize it this way. 
+		int midIndex= inputLength / 2;
+		int [] leftArray= new int[midIndex];
+		int [] rightArray= new int[inputLength - midIndex];//for odd number arrays
+		
+		//We copy the elements of the original input array into the new left and right arrays. 
+		for(int i= 0; i< midIndex; i++) {
+			leftArray[i]= inputArray[i];
+		}
+		
+		for(int i= midIndex; i< inputLength; i++) {
+			leftArray[i]= inputArray[i];
+		}
+	}
+	
 	
 
 }
